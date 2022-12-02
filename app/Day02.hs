c2fc525e2a710d79662208e5cd1b8f38c9f29a6d
Fabian Lippold <fabianlippold1184@gmail.com>
2022-12-02 07:21:36 +0100

    Day 2

--- app/Day02.hs
+++ app/Day02.hs
@@ -4,32 +4,24 @@ module Day02 (p1, p2) where
 
 import Data.Maybe (fromJust)
 
-data RPS
-    = Rock
-    | Paper
-    | Scissors
-    deriving (Eq, Show, Enum)
+data Move = Rock | Paper | Scissors deriving (Eq, Show, Enum)
 
-instance Read RPS where
-    readsPrec :: Int -> ReadS RPS
+instance Read Move where
+    readsPrec :: Int -> ReadS Move
     readsPrec _ "A" = [(Rock, "")]
     readsPrec _ "B" = [(Paper, "")]
     readsPrec _ "C" = [(Scissors, "")]
     readsPrec _ _ = []
 
-data Outcome
-    = Loss
-    | Draw
-    | Win
-    deriving (Eq, Show, Enum)
+data Outcome = Loss | Draw | Win deriving (Eq, Show, Enum)
 
-strategy1 :: [(Char, RPS)]
+strategy1 :: [(Char, Move)]
 strategy1 = [('X', Rock), ('Y', Paper), ('Z', Scissors)]
 
 strategy2 :: [(Char, Outcome)]
 strategy2 = [('X', Loss), ('Y', Draw), ('Z', Win)]
 
-myOutcome :: RPS -> RPS -> Outcome
+myOutcome :: Move -> Move -> Outcome
 myOutcome Rock Paper = Win
 myOutcome Rock Scissors = Loss
 myOutcome Paper Rock = Loss
@@ -38,20 +30,17 @@ myOutcome Scissors Rock = Win
 myOutcome Scissors Paper = Loss
 myOutcome _ _ = Draw
 
-findMove :: Outcome -> RPS -> RPS
-findMove o r = head [m | m <- [Rock ..], myOutcome r m == o]
+findWinMove :: Outcome -> Move -> Move
+findWinMove o r = head [m | m <- [Rock ..], myOutcome r m == o]
 
-myScore :: RPS -> RPS -> Int
-myScore a b = succ (fromEnum b) + (3 * fromEnum (myOutcome a b))
+totalScore :: [(Move, Move)] -> Int
+totalScore = sum . map (\(a, b) -> succ (fromEnum b) + (3 * fromEnum (myOutcome a b)))
 
-totalScore :: [(RPS, RPS)] -> Int
-totalScore = sum . map (uncurry myScore)
-
-input :: [(Char, a)] -> IO [(RPS, a)]
+input :: [(Char, a)] -> IO [(Move, a)]
 input s = map (\[a, _, b] -> (read [a], fromJust $ lookup b s)) . lines <$> readFile "input02.txt"
 
 p1 :: IO Int
 p1 = totalScore <$> input strategy1
 
 p2 :: IO Int
-p2 = totalScore . map (\(m, o) -> (m, findMove o m)) <$> input strategy2
+p2 = totalScore . map (\(m, o) -> (m, findWinMove o m)) <$> input strategy2
