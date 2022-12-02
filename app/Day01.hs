c7aa3f7af8137e48073c1b35cfe648a754ced4fb
Fabian Lippold <fabianlippold1184@gmail.com>
2022-12-02 03:56:17 +0100

    update

--- app/Day01.hs
+++ app/Day01.hs
@@ -1,19 +1,19 @@
 module Day01 (p1, p2) where
 
-import Data.List (groupBy, sortOn)
-import Data.Ord (Down (..))
+import Data.List (groupBy, sort)
 
-input :: IO [[Int]]
+input :: IO [Int]
 input =
-    sortOn (Down . sum)
-        . map (map read)
+    reverse
+        . sort
+        . map (sum . map read)
         . filter (not . any null)
         . groupBy (\a b -> [] `notElem` [a, b])
         . lines
         <$> readFile "input01.txt"
 
 p1 :: IO Int
-p1 = sum . head <$> input
+p1 = head <$> input
 
 p2 :: IO Int
-p2 = sum . map sum . take 3 <$> input
+p2 = sum . take 3 <$> input
