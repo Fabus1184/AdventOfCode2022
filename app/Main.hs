65f7b62457242ea07589595b5de02fc89a61313b
Fabian Lippold <fabianlippold1184@gmail.com>
2022-12-03 17:45:53 +0100

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TupleSections #-}
 
@@ -7,18 +8,31 @@ import qualified Day1
 import qualified Day2
 import qualified Day3
 
+import Control.Monad (when)
 import Control.Monad.Extra (mapMaybeM)
-import Language.Haskell.TH (listE, lookupValueName, stringE, tupE, varE)
-import Language.Haskell.TH.Syntax (showName)
+import Formatting (formatToString, (%))
+import Formatting.ShortFormatters (d)
+import Language.Haskell.TH (integerL, listE, litE, lookupValueName, tupE, varE)
+import System.Environment (getArgs)
 
-solutions :: [(String, IO Int)]
+solutions :: [(Int, Int, IO Int)]
 solutions =
     $( mapMaybeM
-        (\(d, n) -> lookupValueName ("Day" <> show d <> ".p" <> show n))
-        [(d, n) | d <- [1 :: Int .. 25], n <- [1 :: Int, 2]]
-        >>= listE . map (\x -> tupE [stringE (showName x), varE x])
+        (\(day, part) -> ((day,part,) <$>) <$> lookupValueName ("Day" <> show day <> ".p" <> show part))
+        [(day, part) | day <- [1 :: Int .. 25], part <- [1 :: Int, 2]]
+        >>= listE . map (\(day, n, x) -> tupE [litE (integerL (fromIntegral day)), litE (integerL (fromIntegral n)), varE x])
      )
 
 main :: IO ()
 main = do
-    mapM_ (\(n, s) -> s >>= putStrLn . ((n <> ": ") <>) . show) solutions
+    as <- map read <$> getArgs
+    let solutions' =
+            filter
+                ( \(day', part', _) -> case as of
+                    [day, part] -> (day', part') == (day, part)
+                    [day] -> day' == day
+                    _ -> True
+                )
+                solutions
+    when (null solutions') $ error "No solutions"
+    mapM_ (\(d', p', s) -> s >>= putStrLn . formatToString ("Day" % d % " p" % d % ": " % d) d' p') solutions'
