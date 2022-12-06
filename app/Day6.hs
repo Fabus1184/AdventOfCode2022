00bd57d325e78cdf241dcc7c7afa77345fa24437
Fabian Lippold <fabianlippold1184@gmail.com>
2022-12-06 06:46:15 +0100

    update

--- app/Day6.hs
+++ app/Day6.hs
@@ -1,14 +1,11 @@
 module Day6 (p1, p2) where
 
-import Data.List (findIndex, tails)
-import Data.List.Extra (anySame)
-import Data.Maybe (fromJust)
+import Data.List.Extra (anySame, findIndex, tails)
 
 f :: Eq a => Int -> [a] -> String
 f n =
     show
-        . (+ n)
-        . fromJust
+        . maybe 0 (+ n)
         . findIndex (not . anySame)
         . map (take n)
         . tails
