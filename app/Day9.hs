d6b0c04c17e4a443f1d755dc1e808529a3eb2742
Fabian Lippold <fabianlippold1184@gmail.com>
2022-12-09 08:58:15 +0100

    Day 9

--- app/Day9.hs
+++ app/Day9.hs
@@ -4,18 +4,13 @@
 module Day9 (p1, p2) where
 
 import Control.Arrow ((***))
-import Control.Monad (void)
-import Data.Bifunctor (bimap, first, second)
-import Data.Foldable (foldlM)
-import Data.Functor (($>))
+import Data.Bifunctor (first, second)
 import Data.List (nub)
-import Debug.Trace (traceShowId)
 import GHC.IO (unsafePerformIO)
 
 data Direction = U | D | L | R deriving (Show)
 
-type Tail = (Int, Int)
-type Head = (Int, Int)
+type Position = (Int, Int)
 
 instance Read Direction where
     readsPrec :: Int -> ReadS Direction
@@ -26,74 +21,61 @@ instance Read Direction where
         'R' : s -> [(R, s)]
         _ -> []
 
-distance :: (Tail, Head) -> Float
-distance ((x1, y1), (x2, y2)) = sqrt $ fromIntegral $ (x1 - x2) ^ (2 :: Int) + (y1 - y2) ^ (2 :: Int)
-
-data Diagonal = UL | UR | DL | DR deriving (Show)
-
-diagonalDirection :: (Tail, Head) -> Diagonal
-diagonalDirection ((xt, yt), (xh, yh))
-    | xt < xh && yt < yh = UR
-    | xt < xh && yt > yh = DR
-    | xt > xh && yt < yh = UL
-    | xt > xh && yt > yh = DL
-    | otherwise = error "diagonalDirection: impossible"
-
-move :: (Direction, Int) -> (Tail, Head) -> ([Tail], (Tail, Head))
-move (d, n) (t, h)
-    | n == 0 = (,) [] (t, h)
-    | otherwise = first (t' :) $ move (d, pred n) (t', h')
+move :: (Direction, Int) -> Position -> (Position, [Position])
+move (d, n) h@(x, y)
+    | n == 0 = (h, [])
+    | otherwise = second (h' :) $ move (d, pred n) h'
   where
     h' = case d of
-        U -> second succ h
-        D -> second pred h
-        L -> first pred h
-        R -> first succ h
-    t'
-        | distance (t, h) == sqrt 2 && distance (t, h') > sqrt 2 = case diagonalDirection (t, h) of
-            UL -> bimap pred succ t
-            UR -> bimap succ succ t
-            DL -> bimap pred pred t
-            DR -> bimap succ pred t
-        | distance (t, h') > 1 && distance (t, h') /= sqrt 2 = case d of
-            U -> second succ t
-            D -> second pred t
-            L -> first pred t
-            R -> first succ t
-        | otherwise = t
+        U -> (x, succ y)
+        D -> (x, pred y)
+        L -> (pred x, y)
+        R -> (succ x, y)
 
 readInput :: String -> (Direction, Int)
 readInput = (read *** read) . splitAt 1
 
-showGrid :: (Tail, Head) -> String
-showGrid ((x1, y1), (x2, y2)) =
-    let minx = 0
-        maxx = 6
-        miny = 0
-        maxy = 5
+showTrail :: [Position] -> String
+showTrail trail =
+    let minx = -20
+        maxx = 20
+        miny = -20
+        maxy = 20
      in unlines
-            [ [ if x == x1 && y == y1
-                then 'T'
-                else
-                    if x == x2 && y == y2
-                        then 'H'
-                        else
-                            if x == 0 && y == 0
+            . map
+                ( \y ->
+                    map
+                        ( \x ->
+                            if (x, y) == (0, 0)
                                 then 'O'
-                                else '.'
-              | x <- [minx .. maxx]
-              ]
-            | y <- [maxy, pred maxy .. miny]
-            ]
+                                else
+                                    if (x, y) `elem` trail
+                                        then 'X'
+                                        else '.'
+                        )
+                        [minx .. maxx]
+                )
+            $ [maxy, pred maxy .. miny]
 
 test :: String
 test = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2"
 
+test2 :: String
+test2 = "R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20"
+
 p1, p2 :: String -> String
-p1 s = unsafePerformIO $ do
-    let s' = map readInput $ lines s
-    let (t, h) = ((0, 0), (0, 0)) :: (Tail, Head)
-    putStrLn (showGrid (t, h))
-    let k = foldl (\(ts, acc) m -> let (ts', n) = move m acc in (ts ++ ts', n)) ([], (t, h)) s'
-    pure $ show $ length $ nub $ fst k
-p2 = error
+p1 =
+    show
+        . pred
+        . length
+        . nub
+        . fst
+        . first ((0, 0) :)
+        . foldl (\(hs, h) m -> let (h', hs') = move m h in (hs ++ init hs', h')) ([], (0, 0))
+        . map readInput
+        . lines
+p2 s = unsafePerformIO $ do
+    let s' = map readInput $ lines test2
+    let (trail, _) = first (((0, 0) :) . drop 10) (foldl (\(hs, h) m -> let (h', hs') = move m h in (init hs' ++ hs, h')) ([], (0, 0)) s')
+    putStrLn $ showTrail trail
+    error ""
