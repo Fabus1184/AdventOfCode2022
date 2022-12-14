5539e3b73da04b7af1c37470a93ed44a10560f78
Fabian Lippold <fabianlippold1184@gmail.com>
2022-12-14 20:19:39 +0100

    Day 14

--- app/Day14.hs
+++ app/Day14.hs
@@ -4,10 +4,10 @@
 
 module Day14 (p1, p2) where
 
-import Control.Applicative (liftA2)
-import Control.Lens (makeLenses, (%~), (&), (^.))
+import Control.Lens (makeLenses, view, views, (%~), (&), (^.))
+import Control.Monad (liftM2, liftM4)
 import Data.Char (isDigit)
-import Data.List.Extra (maximumOn, nubOrd)
+import Data.List.Extra (find, nubOrd)
 import Data.Set (Set, fromList, insert, notMember, size)
 import Text.ParserCombinators.ReadP (char, munch1, readP_to_S, sepBy, string)
 
@@ -23,34 +23,25 @@ $(makeLenses ''Cave)
 
 instance Read Path where
     readsPrec :: Int -> ReadS Path
-    readsPrec _ = readP_to_S $ Path <$> sepBy (liftA2 (,) (read <$> munch1 isDigit <* char ',') (read <$> munch1 isDigit)) (string " -> ")
+    readsPrec _ = readP_to_S $ Path <$> sepBy (liftM2 (,) (read <$> munch1 isDigit <* char ',') (read <$> munch1 isDigit)) (string " -> ")
 
 readInput :: String -> Cave
-readInput s =
-    let paths = nubOrd $ concatMap (tracePath . read) $ lines s
-     in Cave (fromList paths) (length paths) (succ $ snd $ maximumOn snd paths) 0
+readInput = liftM4 Cave fromList length (succ . maximum . map snd) (const 0) . nubOrd . concatMap (tracePath . read) . lines
   where
     tracePath :: Path -> [Position]
     tracePath (Path ((x1, y1) : (x2, y2) : ps))
         | x1 == x2 = map (x1,) [min y1 y2 .. max y1 y2] ++ tracePath (Path ((x2, y2) : ps))
         | y1 == y2 = map (,y1) [min x1 x2 .. max x1 x2] ++ tracePath (Path ((x2, y2) : ps))
-        | otherwise = error "invalid path"
     tracePath (Path ps) = ps
 
 simulateWhile :: (Cave -> Bool) -> Cave -> Cave
-simulateWhile p c = head $ dropWhile p $ iterate (fallSand (500, 0)) c
-
-fallSand :: Position -> Cave -> Cave
-fallSand (x, y) c
-    | y == c ^. abyss = c & stuff %~ insert (x, y) & nabyss %~ succ
-    | isNotStuff (x, y + 1) = fallSand (x, y + 1) c
-    | isNotStuff (x - 1, y + 1) = fallSand (x - 1, y + 1) c
-    | isNotStuff (x + 1, y + 1) = fallSand (x + 1, y + 1) c
-    | otherwise = c & stuff %~ insert (x, y)
+simulateWhile p = head . dropWhile p . iterate (fallSand (500, 0))
   where
-    isNotStuff :: Position -> Bool
-    isNotStuff p = p `notMember` (c ^. stuff)
+    fallSand :: Position -> Cave -> Cave
+    fallSand (x, y) c'
+        | y == c' ^. abyss = c' & stuff %~ insert (x, y) & nabyss %~ succ
+        | otherwise = maybe (c' & stuff %~ insert (x, y)) (`fallSand` c') (find (`notMember` view stuff c') [(x, succ y), (pred x, succ y), (succ x, succ y)])
 
 p1, p2 :: String -> Int
-p1 = liftA2 (\n -> pred . subtract n . size . (^. stuff)) (^. nRocks) (simulateWhile ((== 0) . (^. nabyss))) . readInput
-p2 = liftA2 (\n -> subtract n . size . (^. stuff)) (^. nRocks) (simulateWhile (((500, 0) `notMember`) . (^. stuff))) . readInput
+p1 = liftM2 (\n -> views stuff $ pred . subtract n . size) (view nRocks) (simulateWhile $ views nabyss (== 0)) . readInput
+p2 = liftM2 (\n -> views stuff $ subtract n . size) (view nRocks) (simulateWhile $ views stuff $ notMember (500, 0)) . readInput
